e8cbe799d1a74af9a9dac115810b8d970be8fa3f
jnakasone <juan.nakasone@gmail.com>
2018-09-08 11:57:25 -0400

    First commit

---
+++ javascript.js
@@ -0,0 +1,28 @@
+//FizzBuzz
+let output = document.getElementById("output");
+
+for(let i = 1; i <=100; i++) {
+	if (i % 3 === 0 && i % 5 ===0) {
+		//output.insertAdjacentHTML('beforeend', '<div>FizzBuzz</div>');
+		let li = document.createElement("li");
+		li.appendChild(document.createTextNode("FizzBuzz"));
+		output.appendChild(li);
+	}
+	else if (i % 3 === 0) {
+		let li = document.createElement("li");
+		li.appendChild(document.createTextNode("Fizz"));
+		output.appendChild(li);
+	}
+	else if (i % 5 === 0) {
+		let li = document.createElement("li");
+		li.appendChild(document.createTextNode("Buzz"));
+		output.appendChild(li);
+	}
+	else {
+		let li = document.createElement("li");
+		li.appendChild(document.createTextNode(i));
+		output.appendChild(li);
+		//output.insertAdjacentHTML('beforeend', '</div>');
+		//console.log(i + "\n");
+	}
+}
